89397f7ec3a5258d6173fbd96475a94feccafa17
SKaplan87 <scottkaplan.sk@gmail.com>
2017-06-07 12:33:22 -0400

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,129 @@
+
+//I want to close any open tabs when you click a different tab
+
+$("#about").click(function () {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            // return $content.is(":visible") ? "About -- And Basic CSS" : "About -- And Basic CSS";
+        });
+    });
+
+});
+$("#qwikist").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Qwikist" : "Qwikist";
+        });
+    });
+
+});
+$("#tmnt").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "TMNT Soccer Shootout" : "TMNT Soccer Shootout";
+        });
+    });
+
+});
+$("#hangman").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Hangman" : "Hangman";
+        });
+    });
+
+});
+$("#planda").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Planda Planning App" : "Planda Planning App";
+        });
+    });
+
+});
+
+$("#conclusion").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Conclusion" : "Conclusion";
+        });
+    });
+
+});
+$("#portfolio").click(function () {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            // return $content.is(":visible") ? "About -- And Basic CSS" : "About -- And Basic CSS";
+        });
+    });
+});
+$("#skills").click(function () {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            // return $content.is(":visible") ? "About -- And Basic CSS" : "About -- And Basic CSS";
+        });
+    });
+});
+// }
+//credit to http://jsfiddle.net/hungerpain/eK8X5/7/
